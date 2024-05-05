abee8f9b09bbc92082d2bae3d591d6e506bf5c10
adair <adair.rios@ingenieria.unam.edu>
2024-05-05 14:13:23 -0600

    Agregar archivos de proyecto.

---
+++ Banco de sangre/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Banco_de_sangre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
